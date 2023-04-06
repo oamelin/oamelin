63ed5772a7c6e2ddcf6878e864e69c6db65cbefa
“Oscar <melinoscar@gmail..com>
2023-04-05 21:23:51 -0300

    mi primer github

---
+++ IntroJava/src/introjava/HolaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CECI
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer= new Scanner(System.in);
+        String nombre;
+        System.out.print("Ingresa tu nombre ");
+        nombre = leer.next();
+        System.out.println("mi nombre es "+nombre+" y estoy aprendiendo java");
+                
+    }
+    
+}
