c538360bafdcf141e284550d957da9629c81e5ad
Oscar Melin <melinoscar@gmail..com>
2023-04-14 19:37:14 -0300

    listo

---
+++ Guia3/src/EjerciciosExtras/Ejercicio2.java
@@ -0,0 +1,15 @@
+/*
+Declarar cuatro variables de tipo entero A, B, C y D y asignarle un valor 
+diferente a cada una. A continuación, realizar las instrucciones necesarias 
+para que: B tome el valor de C, C tome el valor de A, A tome el valor de D y D 
+tome el valor de B. Mostrar los valores iniciales y los valores finales de cada 
+variable. Utilizar sólo una variable auxiliar.
+ */
+package EjerciciosExtras;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
