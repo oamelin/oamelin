9d88afb87c790d0cef9faa71c37fe93f5f428046
Oscar Melin <melinoscar@gmail..com>
2023-04-15 02:52:01 -0300

    listo

--- Guia3/src/EjerciciosExtras/Ejercicio2.java
+++ Guia3/src/EjerciciosExtras/Ejercicio2.java
@@ -7,9 +7,41 @@ variable. Utilizar sólo una variable auxiliar.
  */
 package EjerciciosExtras;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        int A,B,C,D;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingresar valor a las variables:");
+        System.out.print("A: ");
+        A=leer.nextInt();
+        System.out.print("B: ");
+        B=leer.nextInt();
+        System.out.print("C: ");
+        C=leer.nextInt();
+        System.out.print("D: ");
+        D=leer.nextInt();
+        System.out.println("Los valores ingresados son: ");
+        System.out.println("A=>"+A+",B=>"+B+",C=>"+C+",D=>"+D);
+        CAMBIO(A,B,C,D);
+         int AUX;
+        AUX=B;
+        B=C;
+        C=A;
+        A=D; 
+        D=AUX;
+        System.out.println("A=>"+A+",B=>"+B+",C=>"+C+",D=>"+D);
+    }
+
+    public static void CAMBIO(int A , int B, int C, int D) {
+        int AUX;
+        AUX=B;
+        B=C;
+        C=A;
+        A=D; 
+        D=AUX;
+        System.out.println("A=>"+A+",B=>"+B+",C=>"+C+",D=>"+D);
     }
     
 }
