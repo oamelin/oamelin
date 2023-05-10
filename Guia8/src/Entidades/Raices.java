44d6024b7dc1789804eb9383380b09dc078bf86b
Oscar Melin <melinoscar@gmail.com>
2023-05-09 21:58:57 -0300

    Guia8Extras

---
+++ Guia8/src/Entidades/Raices.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Raices {
+    private int a,b,c;    
+
+    public Raices(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    
+}
