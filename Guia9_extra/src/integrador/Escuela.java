d5eb44f95add51692f5346f20a220959d7e77f0a
Oscar Melin <melinoscar@gmail.com>
2023-05-22 19:53:32 -0300

    Integrador

--- Guia9_extra/src/integrador/Escuela.java
+++ Guia9_extra/src/integrador/Escuela.java
@@ -5,13 +5,13 @@ public class Escuela {
         Estudiante[] estudiantes = new Estudiante[8];
         ServicioEstudiante se= new ServicioEstudiante();
 
-        estudiantes[0] = new Estudiante("Juan", 7.5);
-        estudiantes[1] = new Estudiante("María", 8.0);
-        estudiantes[2] = new Estudiante("Pedro", 6.5);
+        estudiantes[0] = new Estudiante("Oscar", 7.5);
+        estudiantes[1] = new Estudiante("PAblo", 8.0);
+        estudiantes[2] = new Estudiante("Jhona", 6.5);
         estudiantes[3] = new Estudiante("Ana", 9.2);
         estudiantes[4] = new Estudiante("Luis", 7.8);
         estudiantes[5] = new Estudiante("Laura", 8.7);
-        estudiantes[6] = new Estudiante("Carlos", 6.9);
+        estudiantes[6] = new Estudiante("Nicolas", 6.9);
         estudiantes[7] = new Estudiante("Sofía", 9.5);
 
         double promedio =se.calcularPromedio(estudiantes);
