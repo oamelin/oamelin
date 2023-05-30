ccdb1c8d03e55332897da405f03d302e6f81df44
Oscar Melin <melinoscar@gmail.com>
2023-05-30 18:58:47 -0300

    NUEVAGUIA

---
+++ Guia10/src/guia10/Guia10.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia10;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author CECI
+ */
+public class Guia10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ejemplos de un arraylist de numeros:
+        ArrayList<Integer> numerosA = new ArrayList();
+        //ejemplo de una linkedList de numeros
+        LinkedList<Integer> numeroB = new LinkedList();
+    
+        //ejemplo de un hashset de cadenas
+        
+        //ejemplo de una treeSet de numeros
+        
+        //ejemplo de un linkedHashSet de cadena:
+        LinkedHashSet>String> frases = new LinkedHashSet();
+    
+    }
+    
+}
